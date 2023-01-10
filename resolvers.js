e15ab5868cfb665fd472f614f2c9de3c47fec811
Ryszard Wolosiuk <ryszard.wolosiuk@ibm.com>
2023-01-10 16:33:54 +0100

    add filtering and sorting

--- resolvers.js
+++ resolvers.js
@@ -1,8 +1,47 @@
 import { Movies } from "./db.js";
 
+const filterByGenre = (movies, byGenre) => {
+    if (byGenre === "All")
+        return movies
+    else
+        return movies.filter(({genres}) => genres.includes(byGenre) )
+    }
+    
+// by title
+const filterByTitle = (movies, byTitle) => {
+    if (!byTitle)
+        return movies
+    else
+        return movies.filter(({title}) => title.toLowerCase().includes(byTitle.toLowerCase()))
+    }
+
+const getFiltered = (_movies, _filter) => {
+    if (!_filter) return _movies
+    const moviesByGenre = filterByGenre(_movies, _filter.byGenre)
+    return filterByTitle(moviesByGenre, _filter.byTitle)
+}
+
+const getSorted = (_movies, _sort) => {
+    if (_sort === 'RUNTIME')
+        _movies.sort((a, b) => b.runtime - a.runtime)
+    else {
+        const getParsedDate = date => Date.parse(date)
+        _movies.sort((a, b) => getParsedDate(a.release_date) - getParsedDate(b.release_date))
+    }
+    return _movies
+}
+
 export const resolvers = {
     Query: {
         movie: (root, {id}) => Movies.findById(id),
-        movies: () => Movies.findAll()
+        movies: (root, {input}) => {
+            if (!input) {
+                return Movies.findAll()
+            }
+            const {filterBy, sortBy} = input
+                return Movies.findAll().then(movies => getFiltered(movies, filterBy))
+                .then(filteredMovies => getSorted(filteredMovies, sortBy))
+        }
+        
     },
   };
